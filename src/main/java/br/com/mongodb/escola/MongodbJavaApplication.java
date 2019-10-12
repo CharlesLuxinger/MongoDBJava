919774e291ac05814fa6ee9ba375f87fc8da71b9
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-12 16:35:38 -0300

    4. Create a Spring Web Project and a Welcome Page

---
+++ src/main/java/br/com/mongodb/escola/MongodbJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mongodb.escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbJavaApplication.class, args);
+	}
+
+}
