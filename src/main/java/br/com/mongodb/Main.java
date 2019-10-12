1bfb1d9a45008592747954a79de4d3dbd0b16c79
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-12 15:52:14 -0300

    2. Insert a new document

--- src/main/java/br/com/mongodb/Main.java
+++ src/main/java/br/com/mongodb/Main.java
@@ -1,5 +1,8 @@
 package br.com.mongodb;
 
+import java.util.Arrays;
+import java.util.Date;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -8,15 +11,35 @@ import com.mongodb.client.MongoDatabase;
 
 public class Main {
 	public static void main(String[] args) {
+		// Connection
 		MongoClient client = new MongoClient();
 
 		MongoDatabase database = client.getDatabase("test");
 
-		MongoCollection<Document> collection = database.getCollection("alunos");
+		MongoCollection<Document> alunos = database.getCollection("alunos");
 
-		Document first = collection.find().first();
+		// Query
 
-		System.out.println(first);
+		Document aluno = alunos.find().first();
+
+		System.out.println(aluno);
+
+		// Insert a new document
+
+		Document newDocument = new Document();
+
+		newDocument.append("nome", "João da Silva")
+		.append("data_nascimento", new Date(1966,10,24))
+		.append("curso", new Document("nome", "Moda"))
+		.append("notas", Arrays.asList(10,9,8))
+		.append("habilidades", Arrays.asList(new Document().append("nome", "Inglês")
+				.append("nível", "Avançado"),
+				new Document().append("nome", "Corte e Costura")
+				.append("nível", "Ninja")));
+
+		// alunos.insertOne(newDocument);
+
+		System.out.println(alunos.count());
 
 		client.close();
 	}
