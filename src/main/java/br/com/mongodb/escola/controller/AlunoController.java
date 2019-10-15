d5156d11e149498200b819f4721be5d24df841c0
Charles Luxinger <charlesluxinger@hotmail.com>
2019-10-14 22:25:24 -0300

    12. Implementing Endpoint aluno/visualizr/id

--- src/main/java/br/com/mongodb/escola/controller/AlunoController.java
+++ src/main/java/br/com/mongodb/escola/controller/AlunoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,6 +26,13 @@ public class AlunoController {
 		List<Aluno> alunos = alunoRepository.findAll();
 		model.addAttribute("alunos", alunos);
 		return "aluno/listar";
+	}
+
+	@GetMapping("visualizar/{id}")
+	public String visualizar(@PathVariable String id, Model model) {
+		Aluno aluno = alunoRepository.findById(id);
+		model.addAttribute("aluno", aluno);
+		return "aluno/visualizar";
 	}
 
 	@GetMapping("/cadastrar")
